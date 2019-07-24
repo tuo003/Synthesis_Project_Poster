26ef34472cb38494145faa97262c5171b635a8f6
Seija Tuomi <seija.tuomi@csiro.au>
2019-07-24 16:22:23 +1000

    first commit

---
+++ Potassium_Isotherms.R
@@ -0,0 +1,16 @@
+install.packages("glue")
+
+install.packages("devtools")
+
+devtools::install_github("brentthorne/posterdown")
+
+libraray(tidyverse)                         
+
+library(gapminder)
+
+library(gganimate)
+library(gifski)
+library(png)
+library(kableExtra)
+
+
